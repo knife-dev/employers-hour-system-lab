6493ae374b608b3618c540637c94a9802a907cf7
knife-s <me@soknife.dev>
2020-04-07 17:30:50 -0300

    abstract removed from IEmployee

--- src/main/java/models/idao/IEmployee.java
+++ src/main/java/models/idao/IEmployee.java
@@ -6,12 +6,12 @@ import main.java.models.idao.IDao;
 public interface IEmployee<T> extends IDao<T> {
 
     // These are class-dependant per employee. 
-    public abstract int taskStart(); // record the start of a task at current time for this employee.
-    public abstract boolean taskEnd(int taskId); // record the end of a task for this employee.
+    public int taskStart(); // record the start of a task at current time for this employee.
+    public boolean taskEnd(int taskId); // record the end of a task for this employee.
 
     // These are most non-dependant per employee. 
-    public abstract int getCurrentMonthTaskHours(); // returns all tasks hours in current mont for this employee.
-    public abstract int getCurrentMonthFee(); // returns current tasks fee for this employee.
-    public abstract int getFee(String start_date, String end_date); // returns current tasks fee for this employee.
+    public int getCurrentMonthTaskHours(); // returns all tasks hours in current mont for this employee.
+    public int getCurrentMonthFee(); // returns current tasks fee for this employee.
+    public int getFee(String start_date, String end_date); // returns current tasks fee for this employee.
 
 }
